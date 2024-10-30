64c31c3a9cef369ed880d73727daa474ee4aa8d7
Anders <as505soreide@gmail.com>
2024-10-31 00:40:17 +0100

    git init :3

---
+++ Scripts/TileMapSelect.cs
@@ -0,0 +1,95 @@
+using Godot;
+using System;
+using System.Collections.Generic;
+
+
+
+public partial class TileMapSelect : TileMap
+{
+	// Called when the node enters the scene tree for the first time.
+	int gridSize = 10;
+
+	Dictionary<Vector2I, int> MapDict = new Dictionary<Vector2I, int>();
+	private PackedScene _beltScene = (PackedScene)GD.Load("res://Objects/conv_belt.tscn");
+	public override void _Ready()
+	{
+		/*
+		Node belt = _beltScene.Instantiate();
+		AddChild(belt);
+		belt.GetNode<AnimatedSprite2D>("Area2D/AnimatedSprite2D").Play();
+		*/
+		//var button = GetNode<TextureButton>("Control/TextureButton");
+		//button.Pressed += OnButtonPressed;
+		
+		for (int x = 0; x < gridSize; x++)
+		{
+			for (int y = 0; y < gridSize; y++)
+			{ 
+				var coords = new Vector2I(x, y);
+				var atlas_coords = new Vector2I(0,0);
+				SetCell(0, coords, 0, atlas_coords, 0);
+				MapDict.Add(coords, 000);
+			}
+		}
+		
+	}
+
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+		//var map = GetNode("res://map.tscn");
+		var tile = LocalToMap(GetGlobalMousePosition());
+
+		Node belt = GetNodeOrNull("uniqueName");
+		if (belt == null){
+			belt = _beltScene.Instantiate();
+			belt.Name = "uniqueName";
+			AddChild(belt);
+			belt.GetNode<AnimatedSprite2D>("Area2D/AnimatedSprite2D").Position = GetGlobalMousePosition();
+		} else {
+			belt.GetNode<AnimatedSprite2D>("Area2D/AnimatedSprite2D").Position = GetGlobalMousePosition();
+		}
+
+		
+		
+
+		for (int x = 0; x < gridSize; x++)
+		{
+			for (int y = 0; y < gridSize; y++)
+			{
+				EraseCell(1, new Vector2I(x, y));
+			}
+		}
+
+		if (MapDict.ContainsKey(tile))
+		{
+			SetCell(1, tile, 1, new Vector2I(0,0), 0);
+
+			
+		}
+	}
+
+    public override void _Input(InputEvent @event)
+    {	
+
+		if (@event is InputEventMouseButton mouseEvent && mouseEvent.Pressed)
+		{
+			/*
+			belt.GetNode<AnimatedSprite2D>("Area2D/AnimatedSprite2D").Play();
+			*/
+
+			MakeInstancedObject(_beltScene, "Area2D/AnimatedSprite2D");
+    	}	
+
+	}
+
+	private void MakeInstancedObject(PackedScene Item, string TexturePath)
+	{
+		Node item_instance = Item.Instantiate();
+		AddChild(item_instance);
+		item_instance.GetNode<AnimatedSprite2D>(TexturePath).Play();
+		var tilePosition = LocalToMap(GetGlobalMousePosition());
+		item_instance.GetNode<AnimatedSprite2D>(TexturePath).Position = tilePosition * 32;
+	}
+}
